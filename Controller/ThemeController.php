60d5bdcd495303c1bc44c884e590252951cd3a2e
Vincent <darkiron90@gmail.com>
2017-11-16 12:04:44 +0100

    Fix controller

--- Controller/ThemeController.php
+++ Controller/ThemeController.php
@@ -7,15 +7,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
-use Netime\ModuleOnePageBundle\Entity\Page;
-use Netime\ModuleOnePageBundle\Form\PageType;
-
 class ThemeController extends Controller{
-
 	public function indexAction(Request $request){
-		return $this->render(
-			'NetimeAdminBundle::index.html.twig',
-			[]
-		);
+		return new Response('Boostrap Theme Bundle',Response::HTTP_OK);	
 	}	
 }
